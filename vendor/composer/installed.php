8fc07e22f48c197093790657b4666e0666e3257c
João Paulo S. Ploger <115250917+TommyYxofPloger@users.noreply.github.com>
2024-05-02 13:23:59 -0300

    idk lol

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a566f6df7f76d9712ebff5f647dbd5aa7aa59983',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a566f6df7f76d9712ebff5f647dbd5aa7aa59983',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
